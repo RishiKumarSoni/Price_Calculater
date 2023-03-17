9ea86629fcb27a8969258067b021c4da35eb82b4
Rishi <soni.rishi006@gmail.com>
2023-03-18 00:46:01 +0530

    added 'clear <' btn and removed placeholder text

--- js/main.js
+++ js/main.js
@@ -6,17 +6,27 @@ addItemRef = document.getElementById("addItem");
 
 finalRef = document.getElementById("Final");
 
-itemWeightRef.value = 0;
-itemRateRef.value = 0;
-addItemRef.value = 0;
+// itemWeightRef.value = 0;
+// itemRateRef.value = 0;
+// addItemRef.value = 0;
 
 function clean(){
-    itemWeightRef.value = 0;
-    itemRateRef.value = 0;
-    addItemRef.value = 0;
+    itemWeightRef.value = '';
+    itemRateRef.value = '';
+    addItemRef.value = '';
     finalRef.value = '';
 }
 
 function make() {
     finalRef.value = Number(itemWeightRef.value) * Number(itemRateRef.value) + Number(addItemRef.value);
+}
+
+function ground(x) {
+    if (x === 'gold') {
+        itemWeightRef.value='';
+    } else if (x === 'rupee') {
+        itemRateRef.value='';
+    } else {
+        addItemRef.value='';
+    }
 }
